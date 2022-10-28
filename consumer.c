c464047a70024789f4f6500ae7aa5e03ce10411a
n-capeta <ncapeta@kent.edu>
2022-10-27 20:49:48 -0400

    Setting up semaphores

--- consumer.c
+++ consumer.c
@@ -10,6 +10,11 @@
 int main(){
     int shm_fd;
     int * table;
+    const char * name = "shared_memory";
+
+    const char * sema1 = "fill";
+    const char * sema2 = "available";
+    const char * sema3 = "mutex";
 
     //Create the shared memory store in description
     shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666)
@@ -19,8 +24,25 @@ int main(){
     //Map the shared memory to my function
     table = mmap(0, sizeof(int), PROT_READ | PROD_WRITE, MAP_SHARED, shm_fd, 0);
 
+    //Creating semaphores
+    //Consumer post filling the table
+    fill = sem_open(sema1, O_CREAT, 0666, 0);
+
+    //Producer post avail after grabbing from consumer
+    avail = sem_open(sema2, O_CREAT, 0666, 3);
+
+    //Mutex for the mutal exclusion on the table
+    mutex = sem_open(seam3, O_CREAT, 0666, 1);
 
 
+
+    //Unlinking & Closing semaphores
+    sem_close(fill);
+    sem_close(avail);
+    sem_close(mutex);
+    sem_unlink(sema1);
+    sem_unlink(sema2);
+    sem_unlink(sema3);
 
     //Removing shared mem
     munmap(table, sizeof(int));
--- producer.c
+++ producer.c
@@ -10,6 +10,12 @@
 int main(){
     int shm_fd;
     int * table;
+    const char * name = "shared_memory";
+
+    const char * sema1= "fill";
+    const char * sema2= "avail";
+    const char * sema3="mutex";
+
 
     //Open the shared mem
     shm_fd = shm_open(name, O_RDWR, 0666);
@@ -17,7 +23,20 @@ int main(){
     //Map the shared map
     table = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
+    //Open semaphores
+    fill = sem_open(sema1, O_CREAT,0666,0);
+    avail = sem_open(sema2, O_CREAT, 0666, 3);
+    mutex = sem_open(sema3,O_CREAT,0666,1);
 
+
+
+    //Unlink & Close semaphores
+    sem_close(fill);
+    sem_close(avail);
+    sem_close(mutex);
+    sem_unlink(sema1);
+    sem_unlink(sema2);
+    sem_unlink(sema3);
 
 
     //Removing shared mem
